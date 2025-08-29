ca3ccef72ee7dd027777a6ca3dcf4fc5576a22c1
Yin <yinnndlovu@gmail.com>
2025-08-29 09:54:08 +0200

    chore: configure the project to successfully run

--- src/main/java/projectred/ProjectRedServer.java
+++ src/main/java/projectred/ProjectRedServer.java
@@ -1,8 +1,11 @@
 package projectred;
 
-public class ProjectRedServer {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class ProjectRedServer {
     public static void main(String[] args) {
-        System.out.println("Hey");
+        SpringApplication.run(ProjectRedServer.class, args);
     }
 }
