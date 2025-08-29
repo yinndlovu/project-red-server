3b877d0681e0979732a9ea97f794cd2ff2bfd132
Yin <yinnndlovu@gmail.com>
2025-08-29 08:37:49 +0200

    Initial commit of project files

---
+++ src/main/java/projectred/ProjectRedServer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package projectred;
+
+/**
+ *
+ * @author 220388239
+ */
+public class ProjectRedServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
