5b6bfadfa88bfcec18951abbcf7e44dc6cd6ee9b
Yin <yinnndlovu@gmail.com>
2025-11-21 18:08:21 +0200

    chore: re-arrange repositories structure

--- ProjectRed.Core/Interfaces/Repositories/IUserRepository.cs
+++ ProjectRed.Core/Interfaces/Repositories/IUserRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjectRed.Core.Interfaces.Auth
+namespace ProjectRed.Core.Interfaces.Repositories
 {
-    internal interface IRegisterService
+    internal interface IUserRepository
     {
     }
 }
